1e151e6804fc31ef6fdd629a1b49ad985eb67ffa
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-16 15:29:58 +0800

    feat(eslintrc): add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-no-undef': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'static-methods',
+          'instance-methods',
+          'instance-variables',
+          'constructor',
+          'render',
+        ],
+      },
+    ],
+  },
 };
