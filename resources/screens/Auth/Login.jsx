125842693648ea8a72ab68c81c917d8fdc19496a
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-17 15:41:25 +0800

    feat(screens): configured initial screens navigation

---
+++ resources/screens/Auth/Login.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum id sed
+        voluptatem dicta amet consectetur et ut, nobis veritatis rerum totam
+        earum assumenda praesentium explicabo reiciendis, recusandae possimus
+        laudantium facilis?
+      </Text>
+    </View>
+  );
+}
---
+++ resources/screens/index.js
@@ -0,0 +1,10 @@
+import Login from './Auth/Login';
+
+const screens = [
+  {
+    name: 'Login',
+    component: Login,
+  },
+];
+
+export default screens;
