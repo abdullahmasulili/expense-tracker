644183843cb26b72973c4e29c50b901209836484
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 07:18:52 +0800

    feat(Login): add hide/show password handler

--- resources/screens/Auth/Login.jsx
+++ resources/screens/Auth/Login.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { View } from 'react-native';
 import { Button, Text, TextInput } from 'react-native-paper';
 
 import styles from './styles';
 
 export default function Login({ navigation }) {
+  const [isSecured, setIsSecured] = useState(true);
+
+  function handleShowPassword() {
+    setIsSecured(isShow => !isShow);
+  }
+
   function handleSubmit() {
     console.log('pressed');
   }
@@ -14,9 +21,13 @@ export default function Login({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Please Login</Text>
+      <Text style={styles.heading}>Sign into your account</Text>
       <TextInput label="Email" keyboardType="email-address" />
-      <TextInput label="Password" />
+      <TextInput
+        label="Password"
+        secureTextEntry={isSecured}
+        right={<TextInput.Icon icon="eye" onPress={handleShowPassword} />}
+      />
       <Button mode="contained" onPress={handleSubmit}>
         Submit
       </Button>
