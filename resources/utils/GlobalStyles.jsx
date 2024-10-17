8d9076c6db4814f86dbb08922df8193053124d94
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 07:51:58 +0800

    feat(utils): add GlobalStyles utilities

---
+++ resources/utils/GlobalStyles.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const GlobalStyles = StyleSheet.create({
+  twoColum: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingRight: 8,
+    gap: 8,
+  },
+  twoColumItem: {
+    width: '50%',
+  },
+});
+
+export default GlobalStyles;
