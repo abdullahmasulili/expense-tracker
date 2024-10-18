5d2e1a2e672a45c4533bfd5f73cea6325b5ce7ef
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 19:35:38 +0800

    feat(components): add expense details dialog

--- resources/components/List/ExpenseItem.jsx
+++ resources/components/List/ExpenseItem.jsx
@@ -1,25 +1,37 @@
+import { useRef } from 'react';
 import { Card, IconButton, MD3Colors } from 'react-native-paper';
+import ExpensesDetail from '../Dialog/ExpensesDetail';
 
 export default function ExpenseItem({ title, summary }) {
+  const dialogRef = useRef();
+
+  function handleShowDetails() {
+    dialogRef.current.open();
+  }
+
   return (
-    <Card mode="contained">
-      <Card.Title title={title} subtitle={summary} />
-      <Card.Actions>
-        <IconButton
-          mode="contained"
-          containerColor={MD3Colors.error80}
-          iconColor={MD3Colors.error50}
-          icon="delete"
-          size={20}
-        />
-        <IconButton
-          mode="contained"
-          containerColor={MD3Colors.primary80}
-          iconColor={MD3Colors.primary50}
-          icon="dots-horizontal-circle-outline"
-          size={20}
-        />
-      </Card.Actions>
-    </Card>
+    <>
+      <ExpensesDetail ref={dialogRef} />
+      <Card mode="contained">
+        <Card.Title title={title} subtitle={summary} />
+        <Card.Actions>
+          <IconButton
+            mode="contained"
+            containerColor={MD3Colors.error80}
+            iconColor={MD3Colors.error50}
+            icon="delete"
+            size={20}
+          />
+          <IconButton
+            mode="contained"
+            containerColor={MD3Colors.primary80}
+            iconColor={MD3Colors.primary50}
+            icon="dots-horizontal-circle-outline"
+            size={20}
+            onPress={handleShowDetails}
+          />
+        </Card.Actions>
+      </Card>
+    </>
   );
 }
