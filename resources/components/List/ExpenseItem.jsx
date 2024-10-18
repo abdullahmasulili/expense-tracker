628b4960802c8d4e2cab7a044fed0d9742cac69e
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 15:37:28 +0800

    patch(ExpenseItem): change card action button using icon button

--- resources/components/List/ExpenseItem.jsx
+++ resources/components/List/ExpenseItem.jsx
@@ -1,12 +1,17 @@
-import { Button, Card } from 'react-native-paper';
+import { Card, IconButton, MD3Colors } from 'react-native-paper';
 
 export default function ExpenseItem({ title, summary }) {
   return (
     <Card mode="contained">
       <Card.Title title={title} subtitle={summary} />
       <Card.Actions>
-        <Button>Delete</Button>
-        <Button>Detail</Button>
+        <IconButton
+          mode="contained"
+          iconColor={MD3Colors.error50}
+          icon="delete"
+          size={20}
+        />
+        <IconButton mode="contained-tonal" icon="more" size={20} />
       </Card.Actions>
     </Card>
   );
