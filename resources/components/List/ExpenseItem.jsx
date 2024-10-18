87feda149166afce57491c76617d4d489388a909
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 15:46:12 +0800

    patch(ExpenseItem): change card action button color

--- resources/components/List/ExpenseItem.jsx
+++ resources/components/List/ExpenseItem.jsx
@@ -7,11 +7,18 @@ export default function ExpenseItem({ title, summary }) {
       <Card.Actions>
         <IconButton
           mode="contained"
+          containerColor={MD3Colors.error80}
           iconColor={MD3Colors.error50}
           icon="delete"
           size={20}
         />
-        <IconButton mode="contained-tonal" icon="more" size={20} />
+        <IconButton
+          mode="contained"
+          containerColor={MD3Colors.primary80}
+          iconColor={MD3Colors.primary50}
+          icon="dots-horizontal-circle-outline"
+          size={20}
+        />
       </Card.Actions>
     </Card>
   );
