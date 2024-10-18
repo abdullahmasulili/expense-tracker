282af754d5346431dd2feb9f358c2cfe28d05717
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 15:26:10 +0800

    feat(ExpenseList): add date picker for filter expense date

--- resources/screens/Expenses/ExpenseList.jsx
+++ resources/screens/Expenses/ExpenseList.jsx
@@ -5,13 +5,27 @@ import { Button, List, Text } from 'react-native-paper';
 import ExpenseItem from '../../components/List/ExpenseItem';
 import DUMMY_DATA from '../../data';
 import styles from './styles';
+import DatePicker from 'react-native-date-picker';
+import { useState } from 'react';
 
 export default function ExpenseList() {
+  const [date, setDate] = useState(new Date());
+  const [isPickDate, setIsPickDate] = useState(false);
+  const formattedDate = date.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  function handleOnDateConfirm(selectedDate) {
+    setDate(selectedDate);
+    setIsPickDate(false);
+  }
   return (
     <ScrollView style={styles.container}>
       <View style={styles.heading}>
         <Text variant="titleLarge">Your Expenses At</Text>
-        <Button>Select Date</Button>
+        <Button onPress={() => setIsPickDate(true)}>{formattedDate}</Button>
       </View>
       <List.Section style={styles.list}>
         {DUMMY_DATA.map((data, index) => (
@@ -22,6 +36,16 @@ export default function ExpenseList() {
           />
         ))}
       </List.Section>
+      <DatePicker
+        modal
+        mode="date"
+        theme="light"
+        maximumDate={new Date()}
+        open={isPickDate}
+        date={date}
+        onCancel={() => setIsPickDate(false)}
+        onConfirm={handleOnDateConfirm}
+      />
     </ScrollView>
   );
 }
