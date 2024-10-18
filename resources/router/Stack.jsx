98fd7d8d171af7cb1187d447e98ee4355c4edada
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 08:35:34 +0800

    patch(Stack): fix initial route

--- resources/router/Stack.jsx
+++ resources/router/Stack.jsx
@@ -8,7 +8,7 @@ export default function MainStack() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false, gestureEnabled: false }}
-      initialRouteName="Auth">
+      initialRouteName="Login">
       {screens.map(screen => (
         <Stack.Screen
           key={screen.name}
