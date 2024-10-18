1c51b0b8a9ed7387cba6f5d3584e7d535a5bfdfc
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-18 14:56:19 +0800

    feat(resources): add dummy data

---
+++ resources/data.js
@@ -0,0 +1,44 @@
+const DUMMY_DATA = [
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+  {
+    year: 2024,
+    month: 'January',
+    expenseSummary: 'Your expense is higher than last month',
+  },
+];
+
+export default DUMMY_DATA;
