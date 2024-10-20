28890304f39f68bba958aa85c827ad109c2f5bdd
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-20 16:42:45 +0800

    refactor(ExpenseCategoryInput): outsource dialog container component

---
+++ resources/components/Dialog/Container.jsx
@@ -0,0 +1,39 @@
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { Dialog, Portal } from 'react-native-paper';
+
+const DialogContainer = forwardRef(function DialogContainer(
+  { title, children },
+  ref,
+) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleOpenDialog() {
+    setIsVisible(true);
+  }
+
+  function handleCloseDialog() {
+    setIsVisible(false);
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        handleOpenDialog();
+      },
+      close() {
+        handleCloseDialog();
+      },
+    };
+  });
+
+  return (
+    <Portal>
+      <Dialog visible={isVisible} onDismiss={handleCloseDialog}>
+        <Dialog.Title>{title}</Dialog.Title>
+        {children}
+      </Dialog>
+    </Portal>
+  );
+});
+
+export default DialogContainer;
--- resources/screens/User/ManageCategory.jsx
+++ resources/screens/User/ManageCategory.jsx
@@ -3,29 +3,69 @@ import { Appbar } from 'react-native-paper';
 import { FlatList } from 'react-native-gesture-handler';
 
 import MenuButton from '../../components/Button/Menu';
-import { EXPENSE_CATEGORIES } from '../Expenses/ExpenseForm';
+import styles from './styles';
+import ExpenseCategoryInput from '../../components/Dialog/ExpenseCategoryInput';
+import { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import DialogContainer from '../../components/Dialog/Container';
 
-function CategoryItem({ category }) {
-  return <MenuButton label={category.label} />;
+function CategoryItem({ category, onPress }) {
+  return <MenuButton label={category.label} onPress={onPress} />;
 }
 
 export default function ManageCategory({ navigation }) {
+  const expenseCategories = useSelector(state => state.expense.categories);
+  const inputDialogRef = useRef();
+  const [dialogTitle, setDialogTitle] = useState('');
+  const [categoryData, setCategoryData] = useState({});
+
   function handleBackAction() {
     navigation.navigate('UserSettings');
   }
 
+  function handleItemPress(category) {
+    setCategoryData(category);
+    setDialogTitle('Edit Category');
+    inputDialogRef.current.open();
+  }
+
+  function handleAddItemPress() {
+    setDialogTitle('Add Category');
+    inputDialogRef.current.open();
+  }
+
+  function handleCloseDialog() {
+    setCategoryData({});
+    inputDialogRef.current.close();
+  }
+
   return (
-    <View>
-      <Appbar.Header>
-        <Appbar.BackAction onPress={handleBackAction} />
-        <Appbar.Content title="Manage Category" />
-        <Appbar.Action icon="plus" />
-      </Appbar.Header>
-      <FlatList
-        data={EXPENSE_CATEGORIES}
-        renderItem={item => <CategoryItem category={item} />}
-        keyExtractor={item => item.value}
-      />
-    </View>
+    <>
+      <DialogContainer ref={inputDialogRef} title={dialogTitle}>
+        <ExpenseCategoryInput
+          category={categoryData}
+          onClose={handleCloseDialog}
+        />
+      </DialogContainer>
+      <View>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={handleBackAction} />
+          <Appbar.Content title="Manage Category" />
+          <Appbar.Action icon="plus" onPress={handleAddItemPress} />
+        </Appbar.Header>
+        <View style={styles.container}>
+          <FlatList
+            data={expenseCategories}
+            renderItem={({ item }) => (
+              <CategoryItem
+                category={item}
+                onPress={() => handleItemPress(item)}
+              />
+            )}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      </View>
+    </>
   );
 }
