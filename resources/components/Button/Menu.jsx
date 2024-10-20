2edb134e16d154474038e3d673cfcf32e3cd1c77
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-20 13:18:36 +0800

    patch(Button/Menu): add to key property for specify aimed screen

--- resources/components/Button/Menu.jsx
+++ resources/components/Button/Menu.jsx
@@ -3,12 +3,12 @@ import { Icon, MD3Colors, Text, TouchableRipple } from 'react-native-paper';
 
 import styles from './styles';
 
-export default function MenuButton({ label, onPress, icon }) {
+export default function MenuButton({ label, onPress, icon, to }) {
   return (
     <TouchableRipple
       style={styles.button}
       rippleColor={MD3Colors.neutral90}
-      onPress={onPress}>
+      onPress={() => onPress(to)}>
       <View style={styles.buttonContent}>
         <Icon source={icon} size={16} />
         <Text variant="bodyLarge">{label}</Text>
