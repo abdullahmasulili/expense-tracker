66f7d54231606634f1b01c8d37ead337548ad20b
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-22 14:34:48 +0800

    patch(TotalExpense): modify props

--- resources/components/Cards/TotalExpense.jsx
+++ resources/components/Cards/TotalExpense.jsx
@@ -1,17 +1,18 @@
 import { Card, Text } from 'react-native-paper';
 import { currency } from '../../utils/Formatter';
 
-export default function TotalExpense({ amount }) {
+export default function TotalExpense({
+  amount,
+  title = 'Your Total Expense At Selected Date',
+  children,
+}) {
   return (
     <Card>
-      <Card.Title
-        title={
-          <Text variant="titleMedium">Your Total Expense At Selected Date</Text>
-        }
-      />
+      <Card.Title title={<Text variant="titleMedium">{title}</Text>} />
       <Card.Content>
         <Text variant="headlineMedium">{currency.format(amount)}</Text>
       </Card.Content>
+      {children}
     </Card>
   );
 }
