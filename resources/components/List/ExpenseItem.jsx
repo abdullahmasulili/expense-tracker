a1c3e6409fe4fe3f633c81746d2052665e6c81a4
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-22 15:09:07 +0800

    patch(ExpenseItem): remove expense detail feature

--- resources/components/List/ExpenseItem.jsx
+++ resources/components/List/ExpenseItem.jsx
@@ -1,25 +1,16 @@
-import { useRef } from 'react';
 import { Card, IconButton, MD3Colors, Text } from 'react-native-paper';
 
 import { currency, formatDate } from '../../utils/Formatter';
 
-import ExpensesDetail from '../Dialog/ExpensesDetail';
 import styles from './styles';
 
 export default function ExpenseItem({ data, onDelete }) {
-  const dialogRef = useRef();
-
-  // function handleShowDetails() {
-  //   dialogRef.current.open();
-  // }
-
   function handleDeletePress() {
     onDelete(data.id);
   }
 
   return (
     <>
-      <ExpensesDetail ref={dialogRef} />
       <Card mode="contained" style={styles.expenseCard}>
         <Card.Title title={formatDate(data.dateTime)} />
         <Card.Content style={styles.cardContent}>
@@ -39,14 +30,6 @@ export default function ExpenseItem({ data, onDelete }) {
             size={20}
             onPress={handleDeletePress}
           />
-          {/* <IconButton
-            mode="contained"
-            containerColor={MD3Colors.primary80}
-            iconColor={MD3Colors.primary50}
-            icon="dots-horizontal-circle-outline"
-            size={20}
-            onPress={handleShowDetails}
-          /> */}
         </Card.Actions>
       </Card>
     </>
